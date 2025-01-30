368b66c4286f67f3e1299baab42f94adfc5c7847
Rahul chhabra <chh_rahul@live.com>
2025-01-30 11:08:49 +0530

    cron for every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('notify:lease-expiration')->daily();
+        $schedule->command('notify:lease-expiration')->everyMinute();
     }
 
     /**
